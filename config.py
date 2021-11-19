ae1653d68807743b60a6247920b9017c9852e29c
tarek-lazaar <tarek.lazaar@cloudreach.com>
2021-11-19 11:00:49 +0100

    config file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = "ttllzz00"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = 'mysql://admin:' + SECRET_KEY + '@database-1.cmcycie0rurf.eu-west-3.rds.amazonaws.com/Library'
     DEBUG = True
     CSRF_ENABLED = True
